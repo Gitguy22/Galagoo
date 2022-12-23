582241ec12d9d3d70b734954b222fc24e9095014
Gitguy22 <113185690+Gitguy22@users.noreply.github.com>
2022-12-23 13:11:51 -0500

    Done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,6 +26,9 @@ public class PlayerController : MonoBehaviour
         // Write the code below to get your Horizontal and Vertical axis
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        
+        Vector2 move = new Vector2(horizontal, vertical);
+        
          if(Input.GetKeyDown(KeyCode.C))
         {
             ShootBullet();
@@ -37,6 +40,14 @@ public class PlayerController : MonoBehaviour
 
         Bullet bullet = bulletObject.GetComponent<Bullet>();
         bullet.Shoot(new Vector2(0,1), 300);
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
     }
     
 }
