21b148277431616c622bf8cb84ab3960b6c3c206
Gitguy22 <113185690+Gitguy22@users.noreply.github.com>
2022-12-23 08:51:22 -0500

    Almost there

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 
 {
-    public GameObject bulletPrefab
+    public GameObject bulletPrefab;
     Rigidbody2D bulletRigidBody;
     // Start is called before the first frame update
     void Awake()
@@ -20,21 +20,11 @@ public class Bullet : MonoBehaviour
     }
     public void Shoot(Vector2 direction, float force)
     {
-        rigidbody2d.AddForce(direction * force)
+        bulletRigidBody.AddForce(direction * force);
     }
-     void ShootBullet()
-    {
-        GameObject bulletObject = Instantiate(bulletPrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
-        Bullet bullet = bulletObject.GetComponent<Bullet>();
-        bullet.Shoot(new Vector2(0,1), 300);
-    }
-     if(Input.GetKeyDown(KeyCode.C))
-        {
-            ShootBullet();
-        }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Destroy(other.GameObject);
+        Destroy(other.gameObject);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,6 +26,17 @@ public class PlayerController : MonoBehaviour
         // Write the code below to get your Horizontal and Vertical axis
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+         if(Input.GetKeyDown(KeyCode.C))
+        {
+            ShootBullet();
+        }
+    }
+    void ShootBullet()
+    {
+        GameObject bulletObject = Instantiate(bulletPrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Bullet bullet = bulletObject.GetComponent<Bullet>();
+        bullet.Shoot(new Vector2(0,1), 300);
     }
     
 }
